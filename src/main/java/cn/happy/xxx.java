70083ec8e52be62d836db336f2b984a9f3a979e8
刘畅 <3298023854@qq.com>
2018-08-03 11:02:36 +0800

    项目初始化完毕

---
+++ src/main/java/cn/happy/xxx.java
@@ -0,0 +1,11 @@
+package cn.happy;
+
+/**
+ * Created by Administrator on 2018-8-3.
+ */
+public class xxx {
+
+    public static void main(String[] args) {
+        System.out.println("123456");
+    }
+}
